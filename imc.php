654faadc5a7e055072f1ccf7eeda587da4fc5405
Valmerio Oliveira de Assis <valmerio@gmail.com>
2021-03-23 22:44:27 +0100

    Altura em centímetros

--- imc.php
+++ imc.php
@@ -2,12 +2,12 @@
 
 function IMC($peso, $altura){
 	
-       $Imc= ($peso)/($altura*$altura);
+       $Imc= ($peso)/($altura*$altura)*10000;
  
        return $Imc;
  
 }
 
-echo IMC(78, 1.67);
+echo IMC(78, 167);
 
 ?>
